24082401e4ac2f3d45cd1d4d0778255e4714ddc6
Shivam Sharma <sharmashivam66151@gmail.com>
2022-02-07 05:32:01 +0000

    1 file commited

--- index.js
+++ index.js
@@ -13,5 +13,21 @@ function  setQuery(evt) {
 }
 
 function getResults(query) {
-
+    fetch(`${api.base}weather?q=${query}&units=metric&APIID=${api.key}`)
+    .then(weather => {
+        return weather.json();
+    }).then(displayResults);
 }
+
+function displayResults(weather) {
+    let city = document.querySelector('.location .city')
+    city.innerText = `${weather.name}, ${weather.sys.country}`;
+
+    let now = new Date();
+    let date = document.querySelector('.location .date')
+    date.innerText = dateBuilder(now);
+}
+
+function dateBuilder (d) {
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
